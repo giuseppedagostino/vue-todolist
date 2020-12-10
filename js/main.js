d9670e4ebdc7e42f2f836c6743650aad7f2658c7
Giuseppe <giuseppe.dagostino.d@gmail.com>
2020-12-10 17:24:34 +0100

    Sezione BONUS completata

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
-// - cancellare un item dalla lista, al click sulla relativa icona 'X'.
 // Bonus:
 // - Oltre al click sul pulsante, consentire l'inserimento del nuovo item anche premendo il tasto Invio della tastiera, dopo aver scritto il testo nel campo di input.
 
 var app = new Vue({
   el: "#root",
   data: {
-    listItem: ["Cose da fare 1","Cose da fare 2","Cose da fare 3"],
+    listItem: ["Studiare Vue","Ripassare Javascript","Tornare a studiare Vue"],
     newItem: "",
   },
   methods: {
-    printNewItem: function() {
+    addNewItem: function() {
       console.log(this.newItem);
       if (this.newItem != "") {
         this.listItem.push(this.newItem);
