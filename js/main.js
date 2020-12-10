0aaf6ba577f6e451c2c2e0196b4d8a35f0ad9b3f
Giuseppe <giuseppe.dagostino.d@gmail.com>
2020-12-10 15:31:02 +0100

    Inserimento dato dell'utente

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-// - aggiungere un campo di input e un pulsante per aggiungere nuove cose da fare alla lista;
 // - cancellare un item dalla lista, al click sulla relativa icona 'X'.
 // Bonus:
 // - Oltre al click sul pulsante, consentire l'inserimento del nuovo item anche premendo il tasto Invio della tastiera, dopo aver scritto il testo nel campo di input.
@@ -12,6 +11,12 @@ var app = new Vue({
   methods: {
     printNewItem: function() {
       console.log(this.newItem);
+      if (this.newItem != "") {
+        this.listItem.push(this.newItem);
+        this.newItem = "";
+      } else {
+        alert("Devi inserire un valore corretto.")
+      }
     },
   }
 });
